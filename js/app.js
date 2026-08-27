2f6d45b76d072d011eca700acb7a90ffb7cc3265
JuanAhumada <ieoshua12@hotmail.com>
2026-08-26 20:31:17 -0600

    Add files via upload
    primera version funciona de la AppCorus

---
+++ js/app.js
@@ -0,0 +1,171 @@
+// ==========================================
+// CONFIGURACIÓN
+// ==========================================
+
+const URL_API =
+"https://script.google.com/macros/s/AKfycbzsdGiPrIZrSBtIrmHyxSuygYHccuCeTC7tn-2DoSCq16QQu88Mv2R5R4TiU-ed6nrE/exec";
+
+
+// ==========================================
+// NAVEGACIÓN
+// ==========================================
+
+function mostrarSeccion(id) {
+
+    document.querySelectorAll(".seccion").forEach(seccion => {
+        seccion.classList.remove("activa");
+    });
+
+    document.getElementById(id).classList.add("activa");
+}
+
+// ==========================================
+// ENSAYOS
+// ==========================================
+
+async function cargarEnsayos() {
+
+    try {
+
+        const response = await fetch(URL_API);
+        const data = await response.json();
+
+        const contenedor =
+            document.getElementById("listaEnsayos");
+
+        contenedor.innerHTML = "";
+
+        data.ensayos.forEach(item => {
+
+            contenedor.innerHTML += `
+                <div class="card">
+                    <h3>📅 ${item.fecha}</h3>
+                    <p>⏰ ${item.hora}</p>
+                    <p>📍 ${item.lugar}</p>
+                </div>
+            `;
+        });
+
+    } catch(error) {
+
+        console.error(error);
+
+    }
+}
+
+// ==========================================
+// ESQUEMAS
+// ==========================================
+
+async function cargarEsquemas() {
+
+    try {
+
+        const response = await fetch(URL_API);
+        const data = await response.json();
+        
+        console.log(data);
+        
+        const contenedor =
+            document.getElementById("listaEsquemas");
+
+        contenedor.innerHTML = "";
+
+        data.esquemas.forEach(item => {
+
+            contenedor.innerHTML += `
+                <div class="card">
+                    <h3>${item.momento}</h3>
+                    <p>🎵 ${item.canto}</p>
+                </div>
+            `;
+        });
+
+    } catch (error) {
+
+        console.error("Error esquemas:", error);
+
+        document.getElementById("listaEsquemas").innerHTML = `
+            <div class="card">
+                Error al cargar los esquemas.
+            </div>
+        `;
+    }
+}
+
+// ==========================================
+// CANTOS
+// ==========================================
+
+async function cargarCantos() {
+
+    try {
+
+        const response = await fetch(URL_API);
+        const data = await response.json();
+
+        console.log("URL:", URL_API);
+        console.log("DATA:", data);
+        console.log("CANTOS:", data.cantos);
+``
+
+        const lista =
+            document.getElementById("listaCantos");
+
+                data.cantos.forEach(canto => {
+
+                    lista.innerHTML += `
+                        <li class="card">
+                            <a href="https://drive.google.com/drive/folders/1wo3zI4_rYH8_HxiYp__dWjxPeDfWamXJ "
+                                🎵 ${canto.url}
+                                    🎵 ${canto.nombre}
+                            </a>
+                            <p>${canto.categoria}</p>
+                        </li>
+                    `;
+
+                });
+
+    } catch(error) {
+
+        console.error("Error cantos:", error);
+
+    }
+}
+// ==========================================
+// FILTRO
+// ==========================================
+
+function filtrarCantos() {
+
+    const texto =
+        document.getElementById("buscarCanto")
+        .value
+        .toLowerCase();
+
+    const elementos =
+        document.querySelectorAll("#listaCantos li");
+
+    elementos.forEach(item => {
+
+        const nombre =
+            item.textContent.toLowerCase();
+
+        item.style.display =
+            nombre.includes(texto)
+                ? ""
+                : "none";
+    });
+}
+
+// ==========================================
+// INICIO
+// ==========================================
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    cargarEnsayos();
+    cargarEsquemas();
+    cargarCantos();
+
+});
